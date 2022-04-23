823c07cf3c8b48bfc4db95b3d7fcc059435bfe60
Aloys Aboge Jr <76847396+AbogeJr@users.noreply.github.com>
2022-04-23 22:45:01 +0300

    crash course: working with lists

--- Hello.py
+++
@@ -1,3 +0,0 @@
-print("Hello World!")
-
-#Adds Python to LinkedIn Skills :)
---
+++ Lists.py
@@ -0,0 +1,20 @@
+# Python Crash Course TIY Exercise 
+print('--------------------------------------------------------------------------')
+# 3-1
+
+friends = ['Eren', 'Levi', 'Sasha', 'Mikasa']
+for friend in friends:
+    print(friend)
+
+print('--------------------------------------------------------------------------')
+# 3-2
+
+for friend in friends:
+    print(f"Good morning {friend}")
+
+print('--------------------------------------------------------------------------')
+# 3-3
+
+cars = ['Mercedes Benz', 'Cupra Leone', 'Audi R6', 'Kia', 'Tesla']
+
+print(f"I would like to own a {cars[-1]}")
