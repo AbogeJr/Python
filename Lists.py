2ad8d6ba0fd07bcff7a2e4c262ce7a82e5ba5c4a
Aloys Aboge Jr <76847396+AbogeJr@users.noreply.github.com>
2022-04-23 23:32:51 +0300

    Finished Crash course: Chapter 4

--- Lists.py
+++ Lists.py
@@ -16,5 +16,91 @@ print('-------------------------------------------------------------------------
 # 3-3
 
 cars = ['Mercedes Benz', 'Cupra Leone', 'Audi R6', 'Kia', 'Tesla']
-
 print(f"I would like to own a {cars[-1]}")
+
+print('--------------------------------------------------------------------------')
+# 3-4
+
+legends = ['Jordan Peterson', 'Albert Camus', 'Elon Musk']
+for legend in legends:
+    print(f"Hey {legend}! Come through!")
+
+print('--------------------------------------------------------------------------')
+# 3-5
+
+dead = 'Albert Camus'
+legends.remove(dead)
+
+legends.insert(1, 'Lex Fridman')
+print(f"{dead} won't be attending for obvious reasons")
+
+for legend in legends:
+    print(f"Hey {legend}! Come through!")
+
+print('--------------------------------------------------------------------------')
+# 3-8
+
+destinations = ['Italy', 'London', 'Rwanda', 'Brasil', 'Germany']
+print(destinations)
+print(sorted(destinations))
+print(destinations)
+print(sorted(destinations, reverse=True))
+print(destinations)
+
+
+print('--------------------------------------------------------------------------')
+# 3-9
+
+print(f"I am inviting {len(legends)} people to dinner")
+
+
+print('--------------------------------------------------------------------------')
+# Numbers List Generation
+even = []
+for num in range(0, 11, 2):
+    even.append(num)
+
+print(even)
+
+squares = []
+for num in range(1, 11):
+    square = num ** 2
+    squares.append(square)
+
+print(squares)
+
+print('--------------------------------------------------------------------------')
+# List Comprehension
+
+cubes = [num ** 3 for num in range(1, 11)]
+print(cubes)
+
+print('--------------------------------------------------------------------------')
+# List Slicing
+racers = ['Ricciardo', 'Occon', 'Hammilton', 'Verstappen', 'Magnussen']
+print("The first three racers are:")
+print(racers[:3])
+print("The Last three racers are:")
+print(racers[-3:])
+
+print('--------------------------------------------------------------------------')
+# List Copying
+my_devices = ['Charger', 'Powerbank', 'Phone']
+her_devices = my_devices[:]
+
+print("Before Copy:")
+print(my_devices)
+print(her_devices)
+
+my_devices.append('Laptop')
+print("After Copy:")
+print(my_devices)
+print(her_devices)
+
+print('--------------------------------------------------------------------------')
+# Tuples
+specials = ('Samosa', 'Caviar', 'Lobster')
+print(specials)
+
+specials = ('Mtura', 'Caviar', 'Lobster')
+print(specials)
--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-# print(friends[0])
-# print(friends[1])
-# print(friends[2])
-# print(friends[3])
