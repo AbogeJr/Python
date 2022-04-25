7477366b4095f9212e6f2c0624b40afbce7aebf1
Aloys Aboge Jr <76847396+AbogeJr@users.noreply.github.com>
2022-04-25 23:24:41 +0300

    PCC: functions & modules

---
+++ modules/main.py
@@ -0,0 +1,4 @@
+from racers import create_racer as cr
+
+racer = cr('Max', 'Verstappen', contract = 'Active', team = 'RedBull', car = 'Aston Martin')
+print(racer)
---
+++ modules/racers.py
@@ -0,0 +1,4 @@
+def create_racer(fname, lname, **profile):
+    profile['first_name'] = fname
+    profile['last_name'] = lname
+    return profile
