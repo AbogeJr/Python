3a464ccd75c418e2e18789bb80fd9f79e21a1df0
Aloys Aboge Jr <76847396+AbogeJr@users.noreply.github.com>
2022-04-27 12:32:21 +0300

    PCC: using json module in file storage

--- StoringData/user.py
+++ StoringData/user.py
@@ -1,16 +1,30 @@
 import json
 
+def getUsername():
+    filename = 'profile.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        username = setUsername()
+    else:
+        return username            
 
-filename = 'profile.json'
 
-
-try:
-    with open(filename) as f:
-        user = json.load(f)   
-except FileNotFoundError:
+def setUsername():
+    filename = 'profile.json'
     username = input("Enter a username: ")
     with open(filename, 'w') as f:
         json.dump(username, f)
-        print(f"We will remember you {username}")  
-else:
-    print(f"Welcome back {user}")
+    return username
+
+def greetUser():
+    username = getUsername()
+    if username:
+        print(f"Welcome back {username}!")
+    else:
+        username = setUsername()
+        print(f"We will remember you {username}")    
+
+
+greetUser()
